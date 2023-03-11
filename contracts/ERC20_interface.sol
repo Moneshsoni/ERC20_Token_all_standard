af02a0a234b3df4b33e2abfe82ea1f42cd29b159
Moneshsoni <sonimonesh115@gmail.com>
2023-03-11 22:15:42 +0530

    add the interface

--- contracts/ERC20_interface.sol
+++ contracts/ERC20_interface.sol
@@ -1,5 +1,13 @@
 pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract ERC20Inteface {
-
+    uint totalsupply;
+    //set the supply 
+    function setSupply(uint _totalSupply)public{
+        totalsupply=_totalSupply;
+    }
+    // get the supply
+    function totalSupply() external view returns (uint256){
+        return totalsupply;
+    }
 }
