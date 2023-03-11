be18c160cbd1c58475549f24c58ed1833607a2ec
Moneshsoni <sonimonesh115@gmail.com>
2023-03-12 00:16:00 +0530

    add the erc20 token and burnable features

---
+++ contracts/erc20Burnable.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.8.9;
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract ERC20Burn is ERC20 ,ERC20Burnable{
+    constructor () ERC20("monesh","ms"){
+
+    }
+
+    function mint(address to, uint256 amount) public  {
+        _mint(to, amount);
+    } 
+}
