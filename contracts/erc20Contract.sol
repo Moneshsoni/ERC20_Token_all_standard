7cde4ad407b690cf692b1aff6d88ff7ae0b2cd0e
Moneshsoni <sonimonesh115@gmail.com>
2023-03-15 17:17:34 +0530

    add the contracct

--- contracts/erc20Contract.sol
+++ contracts/erc20Contract.sol
@@ -2,14 +2,19 @@ pragma solidity 0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract myERC20 is ERC20{
-    address[] public tokenAddress;
+    uint public startDate;
+    uint public endDate;
     constructor(
         string memory name, 
         string memory symbol,
         uint8 decimals, 
-        uint256 initialSupply
+        uint256 initialSupply,
+        uint _startDate,
+        uint _endDate
     ) ERC20(name, symbol) {
         _mint(msg.sender, initialSupply * (10**uint256(decimals)));
+        startDate = _startDate;
+        endDate = _endDate;
     }
 }
 
