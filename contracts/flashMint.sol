4c69c252392ca295c799b4ef76f0fb2f7eec74b0
Moneshsoni <sonimonesh115@gmail.com>
2023-03-17 09:56:18 +0530

    add the flash mint

--- contracts/flashMint.sol
+++ contracts/flashMint.sol
@@ -3,4 +3,15 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";import "@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
 contract FlashMint is ERC20,ERC20FlashMint{
     constructor()ERC20("token","tk"){}
+
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
+    }
+    function deposit()public payable{
+
+    }
+
+    function transferEth()public payable{
+        payable(msg.sender).transfer(msg.value);
+    }
 }
